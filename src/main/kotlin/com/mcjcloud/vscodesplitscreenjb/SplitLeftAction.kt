0db07be89a4e415447c6c56b0a16adb54c527a4a
mcjcloud <brayden14cloud@gmail.com>
2023-08-23 16:15:39 -0500

    update to support IJ 2023.2.1

--- src/main/kotlin/com/mcjcloud/vscodesplitscreenjb/SplitLeftAction.kt
+++ src/main/kotlin/com/mcjcloud/vscodesplitscreenjb/SplitLeftAction.kt
@@ -5,16 +5,33 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.EditorWindow
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
+import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions
 
 class SplitLeftAction : AnAction() {
+//    override fun actionPerformed(e: AnActionEvent) {
+//        val editor: FileEditorManagerImpl = FileEditorManager.getInstance(e.project!!) as FileEditorManagerImpl
+//        val currentWindow = editor.currentWindow
+//        val prevWindow = currentWindow.adjacentEditors[EditorWindow.RelativePosition.LEFT] ?: return
+//
+//        val file = editor.currentFile!!
+//
+//        currentWindow.closeFile(file)
+//        editor.openFileImpl2(prevWindow, file, true)
+//    }
     override fun actionPerformed(e: AnActionEvent) {
         val editor: FileEditorManagerImpl = FileEditorManager.getInstance(e.project!!) as FileEditorManagerImpl
         val currentWindow = editor.currentWindow
-        val prevWindow = currentWindow.adjacentEditors[EditorWindow.RelativePosition.LEFT] ?: return
+        var prevWindow: EditorWindow? = null
+        for (i in 0 until (editor.windows.size)) {
+            if (editor.windows[i] == currentWindow && i > 0) {
+                prevWindow = editor.windows[i - 1]
+            }
+        }
 
         val file = editor.currentFile!!
 
-        currentWindow.closeFile(file)
-        editor.openFileImpl2(prevWindow, file, true)
+        currentWindow?.closeFile(file)
+        editor.openFile(file, prevWindow, FileEditorOpenOptions().withRequestFocus(true))
+        prevWindow?.requestFocus(true)
     }
 }
--- src/main/kotlin/com/mcjcloud/vscodesplitscreenjb/SplitRightAction.kt
+++ src/main/kotlin/com/mcjcloud/vscodesplitscreenjb/SplitRightAction.kt
@@ -5,19 +5,27 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.impl.EditorWindow
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
+import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions
 import javax.swing.JSplitPane
 
 class SplitRightAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val editor: FileEditorManagerImpl = FileEditorManager.getInstance(e.project!!) as FileEditorManagerImpl
-        val currentWindow = editor.currentWindow
-        val nextWindow = currentWindow.adjacentEditors[EditorWindow.RelativePosition.RIGHT]
+        val currentWindow = editor.currentWindow ?: return
+//        val nextWindow = currentWindow.adjacentEditors[EditorWindow.RelativePosition.RIGHT]
+        var nextWindow: EditorWindow? = null
+        for (i in 0 until (editor.windows.size)) {
+            if (editor.windows[i] == currentWindow && i < editor.windows.size - 1) {
+                nextWindow = editor.windows[i + 1]
+            }
+        }
 
         val file = editor.currentFile!!
 
         if (nextWindow != null) {
             currentWindow.closeFile(file)
-            editor.openFileImpl2(nextWindow, file, true)
+            editor.openFile(file, nextWindow, FileEditorOpenOptions().withRequestFocus(true))
+//            editor.openFileImpl2(nextWindow, file, true)
         } else if (currentWindow.files.size > 1) {
             currentWindow.split(JSplitPane.HORIZONTAL_SPLIT, true, file, true)
             currentWindow.closeFile(file)
